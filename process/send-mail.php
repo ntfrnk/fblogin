fa81b87a2c397bf9f8a2e0dadffc375452fbfed3
Franco D. Ocaranza <netfrank777@gmail.com>
2020-08-07 03:35:07 -0300

    Corrección de envio de mail 4

--- process/send-mail.php
+++ process/send-mail.php
@@ -1,6 +1,8 @@
 <?
 
 $header = "From: EPASI Educación <no-reply@epasieducacion.com> \nReply-To: info@epasieducacion.com"."\n";
+$header .= "Mime-Version: 1.0\n";
+$header .= "Content-Type: text/html; charset=utf-8\n";
 
 include("mail/_template.php");
 
